e2ffb28d3968a9364f98c20e825092c0c4703943
Camille-lt <camillelebigot5@gmail.com>
2025-02-12 16:10:44 +0100

    g

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const counterButton = document.querySelector('#counter');
 let count = 0;
 function updateButton () {
-count++
+count++ // count = count + 1
 console.log(count)
 counterButton.innerText = count + ' clics !'
 }
